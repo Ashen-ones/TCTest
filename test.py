c62d966e8a446025134b0ccd91db6f3b2760ced6
Ashen-Ones <kissshot_l@qq.com>
2025-08-17 23:20:52 +0800

    add log and function

--- test.py
+++ test.py
@@ -1,14 +1,20 @@
 import os
-def print_environment_variables():
-    # 获取所有环境变量
-    env_vars = os.environ
 
-    # 打印每个环境变量
+UNREAL_EDITOR_PATH = rf"D:\ue\5.5.1\Engine\Binaries\Win64\UnrealEditor.exe"
+
+def print_environment_variables():
+    env_vars = os.environ
     print("show env var:")
     print("=" * 40)
     for key, value in env_vars.items():
         print(f"env var: {key}: {value}")
+def build_cook_run():
+    print("starr build cook run")
 
+def load_env_var():
+    print("load env par")
 
 if __name__ == "__main__":
     print_environment_variables()
+    load_env_var()
+    build_cook_run()
