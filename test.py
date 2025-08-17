333e2fa116f4882c9ff65dd253a0e92ad11f6130
Ashen-Ones <kissshot_l@qq.com>
2025-08-17 22:03:13 +0800

    1

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("hello world")
+import os
+def print_environment_variables():
+    # 获取所有环境变量
+    env_vars = os.environ
+
+    # 打印每个环境变量
+    print("环境变量列表:")
+    print("=" * 40)
+    for key, value in env_vars.items():
+        print(f"{key}: {value}")
+
+
+if __name__ == "__main__":
+    print_environment_variables()
